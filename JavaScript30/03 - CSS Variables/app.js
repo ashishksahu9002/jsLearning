6b174a9f9367f04654a0827ce813758ec8ff61c2
Ashish Kumar <ashishksahu9002@gmail.com>
2025-07-22 16:03:17 +0530

    day 3 of js using arrow function

--- JavaScript30/03 - CSS Variables/app.js
+++ JavaScript30/03 - CSS Variables/app.js
@@ -1,13 +1,23 @@
-const inputs = document.querySelectorAll('.controls input')
+const inputs = document.querySelectorAll(".controls input");
 
-function handleControls () {
-  // console.log(this.dataset)
-  // console.log(this.name)
-  // console.log(this.value+suffix)
-  const suffix = this.dataset.sizing || '';
-document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+// function handleControls() {
+//   // console.log(this.dataset)
+//   // console.log(this.name)
+//   // console.log(this.value+suffix)
+//   const suffix = this.dataset.sizing || "";
+//   document.documentElement.style.setProperty(
+//     `--${this.name}`,
+//     this.value + suffix
+//   );
+// }
 
-}
+const handleControls = (e) => {
+  const suffix = e.target.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${e.target.name}`,
+    e.target.value + suffix
+  );
+};
 
-inputs.forEach(input => input.addEventListener('change', handleControls))
-inputs.forEach(input => input.addEventListener('mousemove', handleControls))
+inputs.forEach((input) => input.addEventListener("change", handleControls));
+inputs.forEach((input) => input.addEventListener("mousemove", handleControls));
