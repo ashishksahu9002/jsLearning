49f72e77f62c574fa30133beaf974465e3a2856a
Ashish Kumar <ashishksahu9002@gmail.com>
2025-09-18 00:42:21 +0530

    Flatten this deeply nested

---
+++ jsPracticeQuestions/Questions5.js
@@ -0,0 +1,41 @@
+const arr = [1, [2, [3, [4, 5]]]];
+
+// Expected Output: [1, 2, 3, 4, 5]
+
+function flatenRecursive(arr) {
+  let res = [];
+  for (let item of arr) {
+    if (Array.isArray(item)) {
+      res = res.concat(flatenRecursive(item));
+    } else {
+      res.push(item);
+    }
+  }
+  return res;
+}
+
+console.log(flatenRecursive(arr));
+
+function flatenIterative(arr) {
+  let stack = [...arr];
+  let res = [];
+  while (stack.length) {
+    let item = stack.pop();
+    if (Array.isArray(item)) {
+      stack.push(...item);
+    } else {
+      res.push(item);
+    }
+  }
+  return res.reverse();
+}
+
+console.log(flatenIterative(arr));
+
+const out = arr.flat(Infinity);
+console.log(out);
+
+const flattenReduce = (a) =>
+  a.reduce((acc, v) => acc.concat(Array.isArray(v) ? flattenReduce(v) : v), []);
+
+console.log(flattenReduce(arr));
