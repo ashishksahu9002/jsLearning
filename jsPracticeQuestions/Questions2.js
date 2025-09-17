5cf296bff4bf38f01ba20769d4365931a0a89a53
Ashish Kumar <ashishksahu9002@gmail.com>
2025-09-17 23:41:18 +0530

    JS Practice Question2

---
+++ jsPracticeQuestions/Questions2.js
@@ -0,0 +1,25 @@
+const arr = ["apple", "banana", "apple", "orange", "banana", "apple"];
+
+// Expected Output: { apple: 3, banana: 2, orange: 1 }
+
+let output = arr.reduce((acc, curr) => {
+  if (acc[curr]) {
+    acc[curr] += 1;
+  } else {
+    acc[curr] = 1;
+  }
+  return acc;
+}, {});
+console.log(output);
+output = {}
+
+for(let x of arr) {
+  if (output[x]) {
+    output[x] += 1;
+  } else {
+    output[x] = 1;
+  }
+}
+console.log(output);
+
+
