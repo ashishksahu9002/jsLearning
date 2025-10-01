210c02267a8673c50684064d5056dc2fa1947668
Ashish Kumar <76548135+ashishksahu9002@users.noreply.github.com>
2025-10-01 10:01:40 +0530

    Question 2 update

--- jsPracticeQuestions/QuestionList.js
+++ jsPracticeQuestions/QuestionList.js
@@ -1,3 +1,11 @@
+// 1.
 const obj = { a: 1, b: 2, c: 3 };
 
 // Expected Output: [["a", 1], ["b", 2], ["c", 3]]
+
+// 2.
+// Frequency of elements in array
+
+const arr = ["apple", "banana", "apple", "orange", "banana", "apple"];
+
+// Expected Output: { apple: 3, banana: 2, orange: 1 }
